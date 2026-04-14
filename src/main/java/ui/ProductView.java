e8133a2d435da50830336778a567edfdcef61aaf
Elizabeth <elleon@csumb.edu>
2026-04-13 19:54:39 -0700

    Create ui to view products

---
+++ src/main/java/ui/ProductView.java
@@ -0,0 +1,33 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class ProductView extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label titleLabel = new Label("Available Products");
+
+        ListView<String> productList = new ListView<>();
+        productList.getItems().add("Tylenol - $8.99");
+        productList.getItems().add("Advil - $7.99");
+        productList.getItems().add("Benadryl - $6.49");
+
+        VBox root = new VBox(10);
+        root.getChildren().addAll(titleLabel, productList);
+
+        Scene scene = new Scene(root, 300, 250);
+        stage.setTitle("PharmCart Products");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
