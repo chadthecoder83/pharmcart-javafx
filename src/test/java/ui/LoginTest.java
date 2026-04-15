8e2b38f2bb67ecf83bcdbda94881394086dff296
Elizabeth <elleon@csumb.edu>
2026-04-14 23:14:30 -0700

    Update comment login tests

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -3,6 +3,13 @@ package ui;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * PharmCart JavaFX Application
+ * Course: CST 338
+ * @author Elizabeth Leon
+ * @since 4-14-26
+ * Description: Contains unit tests for login validation logic.
+ */
 public class LoginTest {
 
     @Test
