3006214c7efeda4e12f93ac7f6ede57cfdca1ad4
Elizabeth <elleon@csumb.edu>
2026-04-14 23:05:52 -0700

    Add comment for Launcher file

--- src/main/java/ui/Launcher.java
+++ src/main/java/ui/Launcher.java
@@ -3,6 +3,13 @@ package ui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * PharmCart JavaFX Application
+ * Course: CST 338
+ * @author Elizabeth Leon
+ * @since 4-14-26
+ * Description: Entry point of the JavaFX application. Loads the initial login scene.
+ */
 public class Launcher extends Application {
 
     @Override
